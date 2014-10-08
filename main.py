785ead80398c96d62bbc6c906eed404b97250a92
Raymond Hammarling <raha5269@scheele.it.uu.se>
2014-10-08 16:09:31 +0200

    Functions!

--- main.py
+++ main.py
@@ -8,14 +8,31 @@ pygame.display.set_caption("Asteroids!!!")
 done = False
 clock = pygame.time.Clock()
 
-while not done:
+def event():
+    global done
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-    
+
+        # dem bootins
+
+def logic():
+    # logic stuff
+
+    None
+
+def render():
     screen.fill( (255, 0, 0) )
 
+    # render code goes here
+
     pygame.display.flip()
+
+while not done:
+    event()
+    logic()
+    render()
 
     clock.tick(60)
 
