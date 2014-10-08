14677ac0d3892b5e5f6b0edd875bab5a4df2559d
Raymond Hammarling <raha5269@scheele.it.uu.se>
2014-10-08 16:00:58 +0200

    Set up base game stuff

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print 'Hello world?'
+import pygame
+
+pygame.display.init()
+
+screen = pygame.display.set_mode( (640, 480) )
+pygame.display.set_caption("Asteroids!!!")
+
+done = False
+clock = pygame.time.Clock()
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    
+    screen.fill( (255, 0, 0) )
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
